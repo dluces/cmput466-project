93fbedc17914d0043c1690bebdd5f5a95bbdf042
dluces <diego.lucesrosado@gmail.com>
2014-12-03 09:10:33 -0700

    Finished the script to generate crossvalidation datasets. Also, corrected the README.md file so that it reads 'validation' datasets where previously being referred as 'test' datasets.

--- crossvalidation/crossvalidation.py
+++ crossvalidation/crossvalidation.py
@@ -2,10 +2,82 @@ import argparse
 import os
 from pylab import *
 
-def generate(infile, outdir, folds):
+def generate(infile, outdir, k):
 	"""
+	Generates the k-directories inside the outdir.
+	It will then create four files in each directory:
+	A train dataset and its interval file,
+	and a validation dataset and its interval file.
 	"""
-	pass
+
+	# Output for each fold
+	out = [];
+
+	# Prepare the output
+	for i in range(k):
+		out.append([]);
+
+	# Open the input csv dataset
+	with open(infile, newline='') as dataset:
+		# Loop over all the examples
+		data = dataset.readlines();
+		
+	# Shuffle data
+	shuffle(data);
+
+	# Generate the k-folds
+	folds = [data[i::k] for i in range(k)];
+	
+	# Write the datasets
+	for i in range(k):
+		# Validation set will be the current fold
+		validation = folds[i];
+
+		# Validation intervals
+		validationi = set();
+		for example in validation:
+			validationi.add(float(example.split(' ')[0]))
+		validationi = [str(example) for example in sorted(validationi)];
+
+		# Training set will be the examples in the rest of the folds
+		training = [example for fold in folds if fold is not validation
+					for example in fold];
+
+		# Training intervals
+		trainingi = set();
+		for example in training:
+			trainingi.add(float(example.split(' ')[0]))
+		trainingi = [str(example) for example in sorted(trainingi)];
+
+		# Path for current fold
+		path = os.path.abspath(os.path.join(outdir, 'fold'+str(i+1)));
+
+		# Create the directory for the current fold
+		if not os.path.exists(path):
+			os.makedirs(path)
+
+		# Prefix for the filenames
+		filename = os.path.basename(os.path.splitext(infile)[0]);
+		filename = os.path.join(path, filename);
+
+		# Write validation dataset
+		with open(filename + '.valid.data', 'w') as writefile:
+			print(''.join(validation), file=writefile);
+
+		# Write validation intervals
+		with open(filename + '.valid.intervals', 'w') as writefile:
+			print('\r\n'.join(validationi), file=writefile);
+
+		# Write training dataset
+		with open(filename + '.train.data', 'w') as writefile:
+			print(''.join(training), file=writefile);
+
+		# Write training intervals
+		with open(filename + '.train.intervals', 'w') as writefile:
+			print('\r\n'.join(trainingi), file=writefile);
+
+	# No errors
+	return 0;
 
 def main():
 	parser = argparse.ArgumentParser(description='Crossvalidation generator.');
